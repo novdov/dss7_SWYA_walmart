eeab1c11192d8f6626f176bf29c8b4124f003396
novdov <rpwjd56@gmail.com>
2018-04-18 16:21:39 +0900

    moved directory

---
+++ indiviual_dir/wy/datasets_init.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+train = pd.read_csv("../../wy_private/walmart-trip-classification/dataset/train.csv")
+print("train.csv good")
+test = pd.read_csv("../../wy_private/walmart-trip-classification/dataset/test.csv")
+print("test.csv good")
+print("you may now begin")
